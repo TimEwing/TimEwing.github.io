89f672532bffc688515954eecbfbef1e940e8975
Tim Ewing <timjewing@gmail.com>
2022-06-01 00:25:09 -0400

    Add omni, kepler, and nh

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,19 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "title": "nh_convert.sh<span class='blink'>&#9608;</span>",
+        "excerpt":"One of the perks of interning at Southwest Research Institute was being able to do an independent study my senior year. I was asked to convert unresolved observations of Pluto from NASA’s New Horizons between two sets of optical filters. Since the bandwidth profile of the two filter sets didn’t...","categories": [],
         "tags": [],
-        "url": "/jekyll/update/2022/05/23/welcome-to-jekyll.html",
+        "url": "/2022/05/18/nh-convert.html",
+        "teaser": "/assets/images/nh.jpg"
+      },{
+        "title": "kepler.py<span class='blink'>&#9608;</span>",
+        "excerpt":"Kepler is a program which simulates what an Earth-based telescope would see when observing the transit of an exoplanet in front of another solar system’s star. It was designed to generate short, educational animations to help explain the concept to students. I wrote it as an optional, extra-credit assignment for...","categories": [],
+        "tags": [],
+        "url": "/2022/05/21/kepler.html",
         "teaser": null
+      },{
+        "title": "omnichrome.py<span class='blink'>&#9608;</span>",
+        "excerpt":"Omnichrome is a project I’ve been working on for almost as long as I’ve been able to code. I use it as a practice project to make sure I stay in form when I’m not doing much coding at work or school. What does it do? Omnichrome makes images. It...","categories": [],
+        "tags": [],
+        "url": "/2022/05/23/omnichrome.html",
+        "teaser": "/assets/images/omni/splut.png"
       }]
