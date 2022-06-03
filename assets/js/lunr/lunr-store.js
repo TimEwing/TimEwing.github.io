e46fbbe3a7270501e009334a68bbf37f226175de
Tim Ewing <timjewing@gmail.com>
2022-06-03 19:24:00 -0400

    Add SwRI page

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "Southwest Research Institute",
+        "excerpt":"SwRI During my undergraduate degree, I worked part-time at Southwest Research Institure (SwRI) in Boulder, Colorado. The majority of my time was spent on two missions: CYGNSS, a constellation of eight satellites in low Earth orbit, and LUCY, a Discovery class mission on its way to the Trojan asteroids. CYGNSS...","categories": [],
+        "tags": [],
+        "url": "/2022/04/03/swri.html",
+        "teaser": "/assets/images/cygnss.jpg"
+      },{
         "title": "nh_convert.sh<span class='blink'>&#9608;</span>",
         "excerpt":"View source on GitHub One of the perks of interning at Southwest Research Institute was being able to do an independent study my senior year. I was asked to convert unresolved observations of Pluto from NASA’s New Horizons between two sets of optical filters. Since the bandwidth profile of the...","categories": [],
         "tags": [],
